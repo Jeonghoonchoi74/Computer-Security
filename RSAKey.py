85e3b4d0f6a95a38287a6a9a94170b9b128e5a89
Jeonghoonchoi74 <46080119+Jeonghoonchoi74@users.noreply.github.com>
2023-12-06 11:42:26 +0900

    RSA Key

---
+++ RSAKey.py
@@ -0,0 +1,40 @@
+!pip install tinyec
+!pip install pycryptodome
+
+import hashlib
+from hashlib import sha512
+from Crypto.PublicKey import RSA
+
+def fast_modular_exponentiation(a,b,n):
+    result = 1
+    while b>0:
+        if b%2 == 1:
+          result = (result * a) % n
+        a = (a * a) % n
+        b //= 2
+    return result
+
+def rsa_signature_generation(message, d, n):
+  digest = int.from_bytes(hashlib.sha256(message).digest(), byteorder='big')
+  signature = fast_modular_exponentiation(digest, d, n)
+  return signature
+
+def rsa_signature_verification(message, e, n, signature):
+  digest = int.from_bytes(hashlib.sha256(message).digest(), byteorder='big')
+  verification = fast_modular_exponentiation(signature, e, n ) == digest
+  return verification
+
+print("RSA key generation")
+keyPair = RSA.generate(bits = 1024)
+print(" n = ", keyPair.n)
+print(" e = ", keyPair.e)
+print("d = ", keyPair.d)
+
+t = input("message : ")
+msg = bytes(t, 'utf-8')
+
+signature = rsa_signature_generation(msg, keyPair.d, keyPair.n)
+print("signature : ", signature)
+
+verification = rsa_signature_verification(msg, keyPair.e, keyPair.n, signature)
+print("확인결과 : ", verification)
